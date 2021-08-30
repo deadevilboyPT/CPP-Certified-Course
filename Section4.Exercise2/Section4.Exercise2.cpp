7a3aab6b96f21ad585a931baa54016e07c9fdcc7
deadevilboyPT <carva.jman@gmail.com>
2021-08-30 20:40:12 +0100

    Section Challenge

--- Section4.Exercise2/Section4.Exercise2.cpp
+++ Section4.Exercise2/Section4.Exercise2.cpp
@@ -1,11 +1,15 @@
 // Section4.Exercise2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// SECTION CHALLENGE
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int favoriteNr(0);
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favoriteNr;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favoriteNr << " is my favorite number!" << std::endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
