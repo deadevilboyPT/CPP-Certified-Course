31fa763badc6b3d46b3fbf456f93a5eb8f101697
deadevilboyPT <carva.jman@gmail.com>
2021-08-30 18:41:24 +0100

    First Exercise

--- Section4.Exercise1/Section4.Exercise1.cpp
+++ Section4.Exercise1/Section4.Exercise1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello User! This is my first C++ Project of this course!" << std::endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
